71ea9fe818fcede0f919f050dc6d3ef5335cbc39
500a5 <500a5@mail.ru>
2023-10-08 16:20:12 +0300

    init
    init

--- 4 курс 1 сем/чьи то лабы/веб Зановская/web-master/yii/modules/api/ApiModule.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace app\modules\api;
-
-use yii\base\Module;
-
-class ApiModule extends Module
-{
-
-}
--- 4 курс 1 сем/чьи то лабы/веб Зановская/web-master/yii/modules/api/controllers/CategoryController.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace app\modules\api\controllers;
-
-class CategoryController extends BaseActiveController
-{
-    public $modelClass = 'app\models\Category';
-}
